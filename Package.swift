44befeb090cb913147dc9fe81fa0f9aa897ce099
Joannis Orlandos <joannis@orlandos.nl>
2016-04-06 23:45:14 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+    name: "SHA2",
+    dependencies: [
+        .Package(url: "https://github.com/CryptoKitten/CryptoEssentials.git", majorVersion: 0, minor: 2),
+    ]
+)
