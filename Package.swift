7520a639c4663fa911956a77963ef5ba775d7ff1
Yoshiki Kurihara <clouder@gmail.com>
2016-04-12 23:44:34 +0900

    Updated CryptoEssentials

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SHA2",
     dependencies: [
-        .Package(url: "https://github.com/CryptoKitten/CryptoEssentials.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/yoshiki/CryptoEssentials.git", majorVersion: 0, minor: 3),
     ]
 )
